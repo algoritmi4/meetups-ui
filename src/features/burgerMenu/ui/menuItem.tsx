64e09db5c291c50332daacc9b1116a086fec55ee
Elijah <elijah@Elijahs-MacBook-Pro.local>
2024-04-12 16:59:05 +0300

    fix: forwardRef

--- src/features/burgerMenu/ui/menuItem.tsx
+++ src/features/burgerMenu/ui/menuItem.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { IItem } from "../model/types";
 import { Link } from "react-router-dom";
 
-export const MenuItem = ({ img, name, link}: IItem) => {
+export const MenuItem = React.forwardRef(({ img, name, link}: IItem, ref) => {
   return (
-    <Link to={link} className="w-[100%] hover:bg-slate-50">
+    <Link to={link} className="w-[100%] hover:bg-slate-200">
       <div className="flex">
         <div
           className={`${img} w-6 h-6 p-[2.40px] mr-[14px] justify-center items-center inline-flex`}
@@ -14,5 +15,5 @@ export const MenuItem = ({ img, name, link}: IItem) => {
       </div>
     </Link>
   );
-};
+}); 
  
