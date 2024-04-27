341fa2a7d83c6568a84593515ba4648d88481f8d
Elijah <elijah@Elijahs-MacBook-Pro.local>
2024-04-27 10:02:08 +0300

    fix: shadow-burgerMenu

--- src/features/burgerMenu/ui/burgerMenu.tsx
+++ src/features/burgerMenu/ui/burgerMenu.tsx
@@ -12,7 +12,7 @@ export function BurgerMenu() {
   ));
 
   return (
-    <Menu.Items className="absolute flex flex-col right-[25px] top-[12px] z-[11] mt-10 w-[293px] h-[410px] px-10 py-[30px] rounded-[20px] bg-white shadow drop-shadow-bm justify-start items-start gap-[15px] inline-flex focus:outline-none">
+    <Menu.Items className="absolute flex flex-col right-[25px] top-[12px] z-[11] mt-10 w-[293px] h-[410px] px-10 py-[30px] rounded-[20px] bg-white shadow-shadow-bm justify-start items-start gap-[15px] inline-flex focus:outline-none">
       {menuList}
       {refresh && <LogoutButton refresh={refresh} />}
     </Menu.Items>
