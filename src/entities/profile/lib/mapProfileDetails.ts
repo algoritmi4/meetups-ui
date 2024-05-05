544d0f7586e50280983ab5310388bf4baf1b7917
Ivan <ButorinLP@yandex.ru>
2024-05-05 21:54:51 +0300

    fix: fix mapprofiledetails

--- src/entities/profile/lib/mapProfileDetails.ts
+++ src/entities/profile/lib/mapProfileDetails.ts
@@ -1,33 +1,19 @@
-import {type ProfileDetailsDto} from '../api/types'
-import {type ProfileFollowingDto} from '../api/types'
-import {type ProfileDetails} from '../model/types'
-import {type ProfileFollowing} from '../model/types'
-
-
+import {ProfileDetailsDto, type ProfileDetails} from '../model/types'
 
 export function mapProfileDetails(dto: ProfileDetailsDto): ProfileDetails {
-    return {
-        id: dto.id,
-        username: dto.username,
-        email: dto.email,
-        firstName: dto.first_name,
-        lastName: dto.last_name,
-        image: dto.image_url,
-        isEmailVerified: dto.is_email_verified,
-        city: dto.city,
-        is_private: dto.is_private,
-        bio: dto.bio,
-        age: dto.age,
-        date_of_birth: dto.date_of_birth,
-        category_favorite: dto.category_favorite
-    }
-}
-
-export function mapProfileFollowing(dto: ProfileFollowingDto): ProfileFollowing {
-    return {
-        id: dto.id,
-        user: dto.user,
-        follower: dto.follower,
-        status: dto.status,
-    }
+  return {
+    id: dto.id,
+    username: dto.username,
+    email: dto.email,
+    firstName: dto.first_name,
+    lastName: dto.last_name,
+    image: dto.image_url,
+    isEmailVerified: dto.is_email_verified,
+    city: dto.city,
+    is_private: dto.is_private,
+    bio: dto.bio,
+    age: dto.age,
+    date_of_birth: dto.date_of_birth,
+    category_favorite: dto.category_favorite
+  }
 }
