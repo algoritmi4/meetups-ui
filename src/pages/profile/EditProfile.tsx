041f49e387ee0614b90c008c2f0dfd3423abcd49
ElijahWolf1986 <102101100@mail.ru>
2024-05-22 13:17:21 +0300

    fix: eslint error

--- src/pages/profile/EditProfile.tsx
+++ src/pages/profile/EditProfile.tsx
@@ -45,6 +45,7 @@ function EditProfile(): ReactElement {
     } else {
       methods.reset(defaultProfileFormValues);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isProfileDataSuccess]);
 
   if (isProfileDataLoading) {
